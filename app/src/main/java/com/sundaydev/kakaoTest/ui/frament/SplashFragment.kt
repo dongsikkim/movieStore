55851d8c6883a38f38333e06a0ddee873d2a4a82
kimdongsik <simsooltong@gmail.com>
2020-06-15 10:21:47 +0900

    back버튼 선택시 스플래시 미표출되도록 수정

--- app/src/main/java/com/sundaydev/kakaoTest/ui/frament/SplashFragment.kt
+++ app/src/main/java/com/sundaydev/kakaoTest/ui/frament/SplashFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.NavOptions
 import androidx.navigation.fragment.findNavController
 import com.sundaydev.kakaoTest.R
 import com.sundaydev.kakaoTest.databinding.FragmentSplashBinding
@@ -19,6 +20,7 @@ class SplashFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        Handler().postDelayed({ findNavController().navigate(R.id.action_splashFragment_to_movieFragment) }, 1000)
+        val navOptions = NavOptions.Builder().setPopUpTo(R.id.splashFragment, true).build()
+        Handler().postDelayed({ findNavController().navigate(R.id.action_splashFragment_to_movieFragment, null, navOptions) }, 1000)
     }
 }
