b9224e968ad0b89802847b2479a6b2bd97c023cd
김동식 <dongsik.kim@navercopr.com>
2022-11-02 00:55:53 +0900

    - 무한 페이징 API 수정

--- app/src/main/java/com/sundaydev/kakaoTest/datasource/MoviePagingSource.kt
+++ app/src/main/java/com/sundaydev/kakaoTest/datasource/MoviePagingSource.kt
@@ -25,7 +25,7 @@ class MoviePagingSource(
             LoadResult.Page(
                 data = response.results,
                 prevKey = if (nexPage == 1) null else nexPage - 1,
-                nextKey = response.page.plus(1)
+                nextKey = if (params.key == response.total_pages) null else response.page.plus(1)
             )
         } catch (e: Exception) {
             LoadResult.Error(e)
